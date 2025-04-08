3813b04f247f8caf4df02167fd1dd699cca13327
SamJebaxtin <sjebastin7@gmail.com>
2025-04-08 11:22:24 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -84,5 +84,6 @@ if __name__ == '__main__':
     app.run(debug=True)
 	
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 10000))  # Render injects PORT as env variable
+    port = int(os.environ.get('PORT', 5000))  # this is the key line!
     app.run(host='0.0.0.0', port=port)
+
