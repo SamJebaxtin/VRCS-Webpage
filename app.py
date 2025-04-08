b96c89095a0d299119fefdd9b4ed3f2411ada816
SamJebaxtin <sjebastin7@gmail.com>
2025-04-08 11:10:56 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -15,6 +15,14 @@ if not os.path.exists(DEFAULT_SAVE_PATH):
 @app.route('/')
 def index():
     return render_template('index.html')
+	
+@app.route('/')
+def home():
+    return render_template('index.html')  # make sure templates/index.html exists
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 10000))  # Render will set PORT environment variable
+    app.run(host='0.0.0.0', port=port)
 
 
 @app.route('/create_file', methods=['POST'])
